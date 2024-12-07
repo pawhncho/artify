ce37526c088f3da53f9c4b3974f3647de6a05f13
pawhncho <pawhncho@gmail.com>
2024-12-07 02:04:09 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import logo from './logo.png';
 
 function App() {
 	const [prompt, setPrompt] = useState();
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState();
 	const [images, setImages] = useState([
 		{
 			'image': 'https://cdn.pixabay.com/photo/2023/03/28/13/28/ai-generated-7883147_1280.jpg',
@@ -19,6 +21,8 @@ function App() {
 	const generateStory = async () => {
 		const apiUrl = 'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell';
 		const apiToken = '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';
+		setLoading(true);
+		setError();
 
 		try {
 			const response = await axios.post(
@@ -36,11 +40,32 @@ function App() {
 			);
 
 			const imageUrl = URL.createObjectURL(response.data);
-			setImages(imageUrl);
-			console.log(response.data);
+			setImages(images => [...images, { 'image': imageUrl, 'prompt': prompt }]);
 		} catch (error) {
-			// Handle Error
+			if (error.response) {
+				if (error.response.status === 400) {
+					setError('Invalid input data.');
+				} else if (error.response.status === 401) {
+					setError('Invalid authorization token.');
+				} else if (error.response.status === 403) {
+					setError('You have reached your quota limit.');
+				} else if (error.response.status === 404) {
+					setError('Model does not exist.');
+				} else if (error.response.status === 429) {
+					setError('Too many requests. Please slow down.');
+				} else if (error.response.status === 500) {
+					setError('Unexpected server error occurred.');
+				} else if (error.response.status === 503) {
+					setError('Model is currently loading, Try later.');
+				} else {
+					setError('Unknown error occured, Try again.');
+				};
+			} else {
+				setError('Unknown error occured, Try again.');
+			};
 		};
+		setPrompt('');
+		setLoading(false);
 	};
 
 	return (
@@ -54,6 +79,9 @@ function App() {
 
             <main>
             	<section className="input-section">
+            		<div className="input-error">
+            			{error}
+            		</div>
                     <input
                         id="prompt"
                         type="text"
@@ -70,7 +98,7 @@ function App() {
                 <section className="gallery">
                 	<h2 className="visually-hidden">Generated Images</h2>
                 	{
-                		images.map(image => {
+                		images.slice().reverse().map(image => {
                 			return (
                 				<>
                 					<article className="image-card" aria-label="Generated Image 1">
